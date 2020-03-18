779c298e35c296a2484f6711893864f8ab8c6a70
EwaPiechowska <ewapiechowska0@gmail.com>
2020-03-18 22:31:43 +0100

    a

---
+++ zadania domowe/2/improved_1.py
@@ -0,0 +1,27 @@
+
+# Od dziś też będę zwaracał uwagę na to czy nie tylko kod działa ale:
+# a) czy składanie jest ładna i zgodna ze standardami
+# b) czy kod jest zabezpieczony przed wprowadzaniem niepoprawnych danych
+#    (np jeśli prosimy o liczbę a dostaniemy znaki poprośmy użytkownika o podanie danej jeszcze raz)
+# c) czy metody są udokumentowane a co ciekawsze fragmentu kodu opatrzone komentarzem.
+# d) parametryzacja, im większy wpływ na program i jakieś parametry tym fajniej :)
+# e) wydzielanie uniwersalnych fragmentów kodów do osobnych funckji
+
+
+print("Wzór na przeliczanie stopni F na C wygląda tak: F = (32+9) / 5* (C)")
+print("wpisz stopnie w C: ")
+#w przypadku błednego typu danych, program pokazuje komunikat: podaj liczbę!
+try:
+    stopnie_c = float(input())
+    stopnie_f = (32 + 9) / 5 * float(stopnie_c)
+    print("F = " + str(stopnie_f))
+except ValueError:
+    print("Podaj liczbę!")
+
+
+
+
+
+
+
+
---
+++ zadania domowe/2/improved_2.py
@@ -0,0 +1,11 @@
+#2. Napisz program do przeliczania stopni Fahrenheita na Celsjusza (wyświetlając wzór i kolejne obliczenia)
+
+print("Wzór na przeliczanie stopni F na C wygląda tak: c = (F-32)/1.8")
+print("wpisz stopnie w F: ")
+#w przypadku błednego typu danych, program pokazuje komunikat: podaj liczbę!
+try:
+    stopnie_f = float(input())
+    stopnie_c = (stopnie_f - 32) / 1.8
+    print("C = " + str(stopnie_c))
+except ValueError:
+    print("Podaj liczbę!")
