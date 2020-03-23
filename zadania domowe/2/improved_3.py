be694edb30b173b85b8ac9c96e36cac2ed5cd8e1
EwaPiechowska <ewapiechowska0@gmail.com>
2020-03-23 19:15:52 +0100

    a

--- zadania domowe/2/improved_3.py
+++ zadania domowe/2/improved_3.py
@@ -1,11 +1,27 @@
 #3. Napisz program do obliczania pola powierzchni koła o zadanym promieniu (wyświetlając wzór i kolejne obliczenia)
 
-print("Wzór na pole powierzchni koła to: P = πr^2")
+# print("Wzór na pole powierzchni koła to: P = πr^2")
+# print("wpisz długość promienia r: ")
+# try:
+#     r = float(input())
+#     pi = 3.14
+#     P = pi * (r * r)
+#     print(P)
+# except ValueError:
+#     print('błędna wartość podana przez użytkownika')
+
+
+
+def powierzchnia_kola(promien):
+    #3. Napisz program do obliczania pola powierzchni koła o zadanym promieniu (wyświetlając wzór i kolejne obliczenia)
+    print("Wzór na pole powierzchni koła to: P = πr^2")
+    try:
+        r = float(promien)
+        pi = 3.14
+        P = pi * (r * r)
+        print(P)
+    except ValueError:
+        print('błędna wartość podana przez użytkownika')
 print("wpisz długość promienia r: ")
-try:
-    r = float(input())
-    pi = 3.14
-    P = pi * (r * r)
-    print(P)
-except ValueError:
-    print('błędna wartość podana przez użytkownika')
+promien = input()
+powierzchnia_kola(promien)
--- zadania domowe/2/improved_6.py
+++ zadania domowe/2/improved_6.py
@@ -8,12 +8,14 @@ liczba = input()
 def bin2dec(od, do, potega):
     return int(liczba[od:do]) * pow (2, potega)
 
-wynik = bin2dec(0,1,5) + bin2dec(1,2,4) + bin2dec(2,3,3) + bin2dec(3,4,2) + bin2dec(4,5,1) + bin2dec(5,6,0)
+# wynik = bin2dec(0,1,5) + bin2dec(1,2,4) + bin2dec(2,3,3) + bin2dec(3,4,2) + bin2dec(4,5,1) + bin2dec(5,6,0)
+# print(wynik)
+wynik = 0
+#automatycznie numeruje od 0
+#podałam indeks i zakres 6
+for i in range(6):
+    bin2dec(i, i+1 ,5-i)
+    wynik += bin2dec(i, i+1 ,5-i)
 print(wynik)
-
-
-
-
-
 
 
